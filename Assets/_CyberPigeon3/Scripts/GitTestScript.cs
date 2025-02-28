70adabf816e2149c3b9ac2ddb64898e3bc8d5ef0
NandyArnold <143703535+NandyArnold@users.noreply.github.com>
2025-02-28 10:56:45 +0100

    test2

---
+++ Assets/_CyberPigeon3/Scripts/GitTestScript.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class GitTestScript : MonoBehaviour
+{
+    int test = 5;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        test += test;
+        Debug.Log("Frrrrrok  main!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
